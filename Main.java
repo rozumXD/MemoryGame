a92ed9877df3a79bbe27d7f1f66a46b741cfe6d3
rozumXD <109417505+rozumXD@users.noreply.github.com>
2022-07-18 12:42:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,55 +1,80 @@
 import java.io.Console;
 import java.io.File;
-import java.util.Scanner;
-import java.util.Random;
 import java.io.FileNotFoundException;
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.Scanner;
+
+import javax.swing.border.Border;
+
+import org.w3c.dom.TypeInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class Main {
-    public static void main(String[] args) {
-        int choosenLvl = LevelSelector(); 
-        File file = new File("../Words.txt");
-        String path = file.getAbsolutePath();
-        
+    public static void main(String[] args) throws FileNotFoundException{
+       System.out.println("XD");
 
-        
+       int choosenLvl = levelSelector(); 
+       levelInformation(choosenLvl);
+       List words = words();
+       board(words, choosenLvl);
+       
     }
 
-    public static String fileExists() {   //Sprawdzamy czy plik istnieje.
-        File file = new File("Words.txt");
-        boolean exists = file.exists();
-        String path = file.getAbsolutePath();
+    static void board(List words, int choosenLvl){
         
-        return null;
-      
 
-        
-   }
-
-    public static void RunGame(int choosenLvl){   //start gry
-        
         if(choosenLvl==1){
             int bet = 10;
-            System.out.println("\nLevel: easy");
-            System.out.println("\nGuess chances : " +  bet);
-            for(int i=0;i<10;i++){
+            String[][] boardOfWords = new String[2][4];
+            
 
-            }
+            for(int i=0;i<boardOfWords.length;i++){  //wyswietlanie tablicy dwu
+                String[] firstRow = boardOfWords[i];
+                
+                Random rand = new Random();
+                int r = rand.nextInt(100);
 
-        }else if(choosenLvl==2){
-            int bet = 15;
-            System.out.println("\nLevel: hard");
-            System.out.println("\nGuess chances : " +  bet);
-        
+                boardOfWords[1][1] = words.get(r);
+
+                for(int j=0; j<firstRow.length; j++){  
+                    System.out.print(boardOfWords[i][j] + " ");
+                }
+                System.out.println();
+            
+
         }
 
+
+        }else if(choosenLvl==2){
+            
+
+            }
+        }
+        
+    
+
+     static List words() throws FileNotFoundException{  //wczytanie slow z pliku do tablicy arraylist
+        File file = new File("Words.txt");
+        
+            Scanner scanner = new Scanner(file);
+            List<String> words = new ArrayList<>();
+    
+            while(scanner.hasNext()){
+                words.add(scanner.nextLine());
+            }
+           
+        return words;
     }
 
-    public static int LevelSelector(){     
+     static int levelSelector(){     
         Scanner scan = new Scanner(System.in);
         boolean shouldContinue = true;
         int result = 0;
 
-        while(shouldContinue){                                          //wybieranie poziomu trudnosci
+        while(shouldContinue){                                              //wybieranie poziomu trudnosci
             System.out.println("\nChoose the game difficulty level : ");
             System.out.println("[ 1 ] - Easy.");
             System.out.println("[ 2 ] - Hard.");
@@ -62,9 +87,22 @@ public class Main {
                 case 2 -> result = 2;
                 case 3 -> result = 3;  
             }
+
+            shouldContinue = false;
         }
         return result;
     }
 
+ static void levelInformation(int choosenLvl){   //informacja na temat wybranego poziomu
+
+    if(choosenLvl==1){
+        System.out.println("-------------------------------");
+        System.out.println("\nLevel: easy \n") ;
+    }else if(choosenLvl==2){
+        System.out.println("-------------------------------");
+        System.out.println("\nLevel: hard \n") ;
+    }    
+ }
 
 }
+
